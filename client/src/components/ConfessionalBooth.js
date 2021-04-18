56ca633a33609217aae8212f55c71e26d97f8028
Geurt Sengers <geurtsengers@gmail.com>
2021-04-18 12:36:11 +0200

    Add captcha validation.

--- client/src/components/ConfessionalBooth.js
+++ client/src/components/ConfessionalBooth.js
@@ -6,29 +6,48 @@ export class ConfessionalBooth extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            confession: ""
+            confession: "",
+            hasWarning: false
         }
     }
 
     onConfessionChange = (e) => {
         const confession = e.target.value;
-        this.setState(() => ({ confession }));
+        this.setState(() => ({ 
+            confession,
+            hasWarning: false
+        }));
     }
 
     onSubmit = (e) => {
         e.preventDefault();
-        // for now I just take the first few words as a title:
-        const title = this.state.confession
-                            .split(' ')                 // split in words
-                            .slice(0,4)                 // take the first few words
-                            .join(' ')                  // make a string again
-                            .concat('...');             // add ellipses
+        // check for bots:
+        const captcha = this.state.confession.slice(-11)
+        if (captcha !== 'Forgive me.') {
+            this.setState((prevState) => ({
+                confession: prevState.confession,
+                hasWarning: true
+            }))
+        } else {
+            // for now I just take the first few words as a title:
+            const title = this.state.confession
+                                .split(' ')                 // split in words
+                                .slice(0,4)                 // take the first few words
+                                .join(' ')                  // make a string again
+                                .concat('...');             // add ellipses
+    
+            // api call
+            this.props.postConfession({
+                title,
+                confession: this.state.confession
+            });
+            // clear form
+            this.setState(() => ({
+                confession: '',
+                hasWarning: false
+            }))
+        }
 
-        // api call
-        this.props.postConfession({
-            title,
-            confession: this.state.confession
-        });
     }
 
     render() {
@@ -36,10 +55,11 @@ export class ConfessionalBooth extends React.Component {
             <form className="confessional-booth" onSubmit={this.onSubmit}>
                 <textarea
                     onChange = {this.onConfessionChange}
-                    className="confessional-booth__text" 
+                    className={this.state.confession.length > 0 ? "confessional-booth__text active" : "confessional-booth__text"}
                     placeholder="confess"
                     value={this.state.confession}
                 />
+                {this.state.hasWarning && <p className="confessional-booth__capthca-warning">Confessions must end with the phrase: Forgive me. Because bots will never be forgiven.</p>}
                 <button className="confessional-booth__button">repent</button>
             </form>
         )
